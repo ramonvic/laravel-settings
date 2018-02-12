8f2f30120bb0c2d0f8ac9a536d3fb451464b53c2
Ramon Vicente <ramonvicentesilva@hotmail.com>
2018-02-12 16:42:00 -0200

    Fix documentation of Storage Contract

--- src/Setting/SettingStorageContract.php
+++ src/Setting/SettingStorageContract.php
@@ -12,12 +12,19 @@ interface SettingStorageContract
     public static function all();
 
     /**
-     * Return setting value or default value by key.
+     * Return all settings rows
+     * @param null $lang
+     * @return mixed
+     */
+    public function retrieveAll($lang = null);
+
+    /**
+     * Return setting value by key and optional by lang.
      *
      * @param string $key
-     * @param string $value
+     * @param string $lang
      *
-     * @return string
+     * @return string|null
      */
     public function retrieve($key, $lang = null);
 
@@ -26,15 +33,18 @@ interface SettingStorageContract
      *
      * @param string $key
      * @param mixed  $value
+     * @param string $lang
      *
      * @return void
      */
     public function store($key, $value, $lang);
 
     /**
-     * Check if the setting exists.
+     * Modify a setting.
      *
      * @param string $key
+     * @param string $value
+     * @param string $lang
      *
      * @return bool
      */
@@ -44,6 +54,7 @@ interface SettingStorageContract
      * Delete a setting.
      *
      * @param string $key
+     * @param string $lang
      *
      * @return void
      */
