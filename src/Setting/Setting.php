de529ce5776fa4af2c30b5f72e8e93fc073f8da3
Ramon Vicente <ramonvicentesilva@hotmail.com>
2018-02-12 16:48:28 -0200

    Major changes in how settings is obtained, added use of casts, mutators, removed cache, only one request is make on database on initial binding

--- src/Setting/Setting.php
+++ src/Setting/Setting.php
@@ -2,71 +2,45 @@
 
 namespace Unisharp\Setting;
 
-use Illuminate\Contracts\Cache\Factory as CacheContract;
+use Illuminate\Database\Eloquent\Concerns\HasAttributes;
 
 class Setting
 {
+    public static $castMap = [];
+
+    use HasAttributes;
+
     protected $lang = null;
     protected $autoResetLang = true;
-    protected $storage = null;
-    protected $cache = null;
 
-    public function __construct(SettingStorageContract $storage, CacheContract $cache)
+    protected $storage = null;
+
+    public function __construct(SettingStorageContract $storage)
     {
         $this->storage = $storage;
-        $this->cache = $cache;
+        if (\Schema::hasTable('settings')) {
+            $this->load();
+        }
     }
 
-    public function all()
+    private function load()
     {
-        return $this->storage->all();
-    }
-
-    /**
-     * Return setting value or default value by key.
-     *
-     * @param string $key
-     * @param string $value
-     *
-     * @return string|null
-     */
-    public function get($key, $default_value = null)
-    {
-        if (strpos($key, '.') !== false) {
-            $setting = $this->getSubValue($key);
-        } else {
-            if ($this->hasByKey($key)) {
-                $setting = $this->getByKey($key);
-            } else {
-                $setting = $default_value;
+        $settings = $this->storage->retrieveAll($this->lang);
+        $attributes = [];
+        foreach ($settings as $setting) {
+            $value = json_decode($setting->value, true);
+            if (!is_array($value)) {
+                $value = $setting->value;
             }
-        }
-        $this->resetLang();
-
-        if (is_null($setting)) {
-            $setting = $default_value;
+            array_set($attributes, $setting->key, $value);
         }
 
-        return $setting;
-    }
+        $attributes = array_dot($attributes);
 
-    /**
-     * Set the setting by key and value.
-     *
-     * @param string $key
-     * @param mixed  $value
-     *
-     * @return void
-     */
-    public function set($key, $value)
-    {
-        if (strpos($key, '.') !== false) {
-            $this->setSubValue($key, $value);
-        } else {
-            $this->setByKey($key, $value);
+        foreach ($attributes as $key=>$value) {
+            $this->setAttribute($key, $value);
         }
-
-        $this->resetLang();
+        $this->syncOriginal();
     }
 
     /**
@@ -78,186 +52,74 @@ class Setting
      */
     public function has($key)
     {
-        $exists = $this->hasByKey($key);
-
-        $this->resetLang();
+        $exists = $this->has($key);
 
         return $exists;
     }
 
-    /**
-     * Delete a setting.
-     *
-     * @param string $key
-     *
-     * @return void
-     */
-    public function forget($key)
+    public function get($key = null)
     {
-        if (strpos($key, '.') !== false) {
-            $this->forgetSubKey($key);
-        } else {
-            $this->forgetByKey($key);
+        if (!is_null($key)) {
+            return $this->getAttribute($key);
         }
-
-        $this->resetLang();
+        return $this->attributesToArray();
     }
 
-    /**
-     * Should language parameter auto retested ?
-     *
-     * @param bool $option
-     *
-     * @return instance of Setting
-     */
-    public function langResetting($option = false)
+    public function set($key, $value)
     {
-        $this->autoResetLang = $option;
-
-        return $this;
+        $this->setAttribute($key, $value);
     }
 
-    /**
-     * Set the language to work with other functions.
-     *
-     * @param string $language
-     *
-     * @return instance of Setting
-     */
-    public function lang($language)
+    public function __get($name)
     {
-        if (empty($language)) {
-            $this->resetLang();
-        } else {
-            $this->lang = $language;
-        }
-
-        return $this;
+        return $this->getAttribute($name);
     }
 
-    /**
-     * Reset the language so we could switch to other local.
-     *
-     * @param bool $force
-     *
-     * @return instance of Setting
-     */
-    protected function resetLang($force = false)
+    public function __set($key, $value)
     {
-        if ($this->autoResetLang || $force) {
-            $this->lang = null;
-        }
-
-        return $this;
+        $this->set($key, $value);
     }
 
-    protected function getByKey($key)
-    {
-        if (strpos($key, '.') !== false) {
-            $main_key = explode('.', $key)[0];
-        } else {
-            $main_key = $key;
-        }
-
-        if ($this->cache->has($main_key.'@'.$this->lang)) {
-            $setting = $this->cache->get($main_key.'@'.$this->lang);
-        } else {
-            $setting = $this->storage->retrieve($main_key, $this->lang);
-
-            if (!is_null($setting)) {
-                $setting = $setting->value;
-            }
-
-            $setting_array = json_decode($setting, true);
-
-            if (is_array($setting_array)) {
-                $setting = $setting_array;
-            }
-
-            $this->cache->add($main_key.'@'.$this->lang, $setting, 1);
-        }
-
-        return $setting;
-    }
-
-    protected function setByKey($key, $value)
-    {
-        if (is_array($value)) {
-            $value = json_encode($value);
-        }
-
-        $main_key = explode('.', $key)[0];
-
-        if ($this->hasByKey($main_key)) {
-            $this->storage->modify($main_key, $value, $this->lang);
-        } else {
-            $this->storage->store($main_key, $value, $this->lang);
-        }
-
-        if ($this->cache->has($main_key.'@'.$this->lang)) {
-            $this->cache->forget($main_key.'@'.$this->lang);
-        }
-    }
-
-    protected function hasByKey($key)
-    {
-        if (strpos($key, '.') !== false) {
-            $setting = $this->getSubValue($key);
-        } else {
-            if ($this->cache->has($key.'@'.$this->lang)) {
-                $setting = $this->cache->get($key.'@'.$this->lang);
+    public function save() {
+        foreach ($this->getDirty() as $key=>$value) {
+            if ($this->storage->retrieve($key, $this->lang)) {
+                $this->storage->modify($key, $value, $this->lang);
             } else {
-                $setting = $this->storage->retrieve($key, $this->lang);
+                $this->storage->store($key, $value, $this->lang);
             }
         }
-        return ($setting === null) ? false : true;
+        $this->syncOriginal();
     }
 
-    protected function forgetByKey($key)
+    public static function castMap(array $map = null, $merge = true)
     {
-        $this->storage->forget($key, $this->lang);
-
-        $this->cache->forget($key.'@'.$this->lang);
+        if (is_array($map)) {
+            static::$castMap = $merge && static::$castMap
+                ? $map + static::$castMap : $map;
+        }
+        return static::$castMap;
     }
 
-    protected function getSubValue($key)
+    public function getCasts()
     {
-        $setting = $this->getByKey($key);
-
-        $subkey = $this->removeMainKey($key);
-
-        $setting = array_get($setting, $subkey);
-
-        return $setting;
+        return static::$castMap ?? [];
     }
 
-    protected function setSubValue($key, $new_value)
+    public function getAttribute($key)
     {
-        $setting = $this->getByKey($key);
-
-        $subkey = $this->removeMainKey($key);
-
-        array_set($setting, $subkey, $new_value);
-
-        $this->setByKey($key, $setting);
+        if ($key && array_key_exists($key, $this->attributes) ||
+            $this->hasGetMutator($key)) {
+            return $this->getAttributeValue($key);
+        }
+        return null;
     }
 
-    protected function forgetSubKey($key)
+    public function toJson()
     {
-        $setting = $this->getByKey($key);
-
-        $subkey = $this->removeMainKey($key);
-
-        array_forget($setting, $subkey);
-
-        $this->setByKey($key, $setting);
+        return \GuzzleHttp\json_encode($this->attributesToArray());
     }
 
-    protected function removeMainKey($key)
-    {
-        $pos = strpos($key, '.');
-        $subkey = substr($key, $pos + 1);
-
-        return $subkey;
-    }
+    public function getDates(){ return []; }
+    public function getVisible(){ return []; }
+    public function getHidden(){ return []; }
 }
